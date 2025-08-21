2f1d42a38a158df5ca28bb9a0bf8afd2ceb81051
sunfloweerr5 <defa05321@gmail.com>
2025-08-21 17:49:39 +0700

    21/8

--- React-WebApp/src/App.jsx
+++ React-WebApp/src/App.jsx
@@ -1,38 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import Navbar from './Components/Navbar'
-import Footer from './Components/Footer'
-function App() {
-  const [count, setCount] = useState(0)
+import MainLayout from "./Layouts/MainLayout";
+import AboutPage from "./Pages/AboutPage";
+import HomePage from "./Pages/HomePage";
+import NotFound from "./Pages/NotFound";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+function App() { 
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path = "/"element= {<MainLayout /> }>
+      <Route index element = {<HomePage/>}/>
+      <Route path= "/about" element={<AboutPage />}/>
+      <Route path= "*" element={<NotFound />}/>
 
-  return (
-    <>
-    <Navbar/>
-    <Footer/>
-     <h1 class="text-3xl font-bold underline text-rose-400">Hello world! </h1>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      </Route>
+    )
   )
+  return <RouterProvider router={router}/>
 }
 
-export default App
+export default App;
--- React-WebApp/src/Components/Navbar.jsx
+++ React-WebApp/src/Components/Navbar.jsx
@@ -1,15 +1,37 @@
-import React from 'react'
-import reactLogo from '../assets/react.svg'
+import React from "react";
+import reactLogo from "../assets/react.svg";
+import { Link, NavLink } from "react-router-dom";
 
 function Navbar() {
+  function activeLink({ isActive }) {
+    return isActive
+      ? "bg-blue-700 bg-opacity-60 rounded px-3 py-2"
+      : "hover:bg-blue-700 hover:bg-opacity-20 rounded px-3 py-2";
+  }
+
   return (
     <header>
- <div className="flex mr-auto gap-x-2 font-semibold text-2xl"> <img src={reactLogo} alt="React Logo" />
- React : TODO List
-    <img src={reactLogo} className="logo react" alt="React logo" />
- </div>
- </header>
-  )
+      <div className="flex mr-auto gap-x-2 font-semibold text-2xl">
+        {" "}
+        <img src={reactLogo} alt="React Logo" />
+        React : TODO List
+        <img src={reactLogo} className="logo react" alt="React logo" />
+      </div>
+      <ul className="hidden md:flex gap-x-6">
+        <li>
+          <NavLink to="/" className={activeLink}>
+            {" "}
+            Home{" "}
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/about" className={activeLink}>
+            About
+          </NavLink>
+        </li>
+      </ul>
+    </header>
+  );
 }
 
-export default Navbar
+export default Navbar;
