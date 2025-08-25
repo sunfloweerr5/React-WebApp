098641097dd7c267ee650122a396da4a86118922
sunfloweerr5 <defa05321@gmail.com>
2025-08-25 17:25:49 +0700

    25/8/17.25

--- React-WebApp/src/App.jsx
+++ React-WebApp/src/App.jsx
@@ -14,7 +14,7 @@ function App() {
       <Route path = "/"element= {<MainLayout /> }>
       <Route index element = {<HomePage/>}/>
       <Route path= "/about" element={<AboutPage />}/>
-      <Route path= "*" element={<NotFound />}/>
+      <Route path= "*" element={<NotFound />}/>     {/* path คือหน้าที่ไม่ได้ระบุบ path จะให้ขึ้น 404  */}
 
       </Route>
     )
--- React-WebApp/src/Components/NewTask.jsx
+++ React-WebApp/src/Components/NewTask.jsx
@@ -1,21 +1,23 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 
 function NewTask({ addTask }) {
-  const [title, setTitle] = useState("");
-  const submitForm = (e) => {
-    e.preventDefault();
+  //   const [title, setTitle] = useState("");
+  const title = useRef();     //ประกาศตัวแปร title เพื่ออ้างถึง input field
+  const form = useRef();      //ประกาศตัวแปร form เพื่ออ้างถึง form element 
+  const submitForm = (e) => {   //  ฟังก์ชั่นจัดการ summit form
+    e.preventDefault();       //ป้องกันการรีเฟรชหน้าเว็ปเมื่อกด summit
+                               
 
-    const task = {
-      title,
+    const task = {            //สร้างอ็อปเจ็กต์ task ใหม่
+      title: title.current.value,
       date: new Date().toLocaleString(),
     };
-
-    addTask(task);
+    addTask(task);          //เรียกใช้ฟังก์ชั่น 
+    form.current.reset();
   };
-
   return (
     <>
-      <form onSubmit={submitForm}>
+      <form ref={form} onSubmit={submitForm}>
         <label htmlFor="title" className="text-lg text-gray-400">
           Add New Task
         </label>
@@ -28,8 +30,9 @@ function NewTask({ addTask }) {
             placeholder="Type Something here..."
             autoFocus
             required
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
+            ref={title}         //เชื่อมโยง input field กับตัวแปร title
+            // value={title}
+            // onChange={(e) => setTitle(e.target.value)}
           />
           <button
             type="submit"
